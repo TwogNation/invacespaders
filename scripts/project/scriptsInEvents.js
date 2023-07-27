e8a762247cae482d7ecbf39e00295930f37ce52f
Matheus N <matheusnaschaves@gmail.com>
2023-07-27 18:16:29 -0300

    ws

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -8,9 +8,66 @@ function postText(value){
 
 const scriptsInEvents = {
 
+	async Gamesheet_Event2_Act9(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.onMessage = (event) => {
+			if (event.data.startsWith('s:')){
+				const score_ = event.data.split(':')[1];
+				console.log(score_);
+				runtime.globalVars.score = score_;
+				runtime.callFunction("updateScore")
+			}
+		}
+	},
+
 	async Gamesheet_Event28_Act13(runtime, localVars)
 	{
-		postText(runtime.globalVars.postMsg)
+		runtime.globalVars.webSocket.close();
+	},
+
+	async Gamesheet_Event82_Act2(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:10") 
+	},
+
+	async Homesheet_Event15_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('startendless');
+			};
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
+	},
+
+	async Homesheet_Event24_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('ws://localhost:4040/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('startendless');
+			};
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
 	}
 
 };
